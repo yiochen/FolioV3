6914517d2e507e2510460eb5e805a056c997080f
yiochen <chen.yiou1992@gmail.com>
2016-12-25 10:32:30 -0500

    fixed scroll compatibility issue

--- src/services/scroll-service.js
+++ src/services/scroll-service.js
@@ -1,6 +1,15 @@
 export default function smooth_scroll_to (target, duration) {
     target = Math.round(target);
     duration = Math.round(duration);
+     var getScrollTop = function () {
+        return Math.max(window.pageYOffset, document.documentElement.scrollTop, document.body.scrollTop);
+    }
+
+    var setScrollTop = function (value) {
+        window.pageYOffset = value;
+        document.documentElement.scrollTop = value;
+        document.body.scrollTop = value;
+    }
     if (duration < 0) {
         return Promise.reject("bad duration");
     }
@@ -12,9 +21,9 @@ export default function smooth_scroll_to (target, duration) {
     var start_time = Date.now();
     var end_time = start_time + duration;
 
-    var start_top = document.body.scrollTop;
+    var start_top = getScrollTop();
     var distance = target - start_top;
-
+   
     // based on http://en.wikipedia.org/wiki/Smoothstep
     var smooth_step = function(start, end, point) {
         if(point <= start) { return 0; }
@@ -26,11 +35,11 @@ export default function smooth_scroll_to (target, duration) {
     return new Promise(function(resolve, reject) {
         // This is to keep track of where the document.body's scrollTop is
         // supposed to be, based on what we're doing
-        var previous_top = document.body.scrollTop;
+        var previous_top = getScrollTop();
 
         // This is like a think function from a game loop
         var scroll_frame = function() {
-            if(document.body.scrollTop != previous_top) {
+            if(getScrollTop() != previous_top) {
                 reject("interrupted");
                 return;
             }
@@ -39,7 +48,7 @@ export default function smooth_scroll_to (target, duration) {
             var now = Date.now();
             var point = smooth_step(start_time, end_time, now);
             var frameTop = Math.round(start_top + (distance * point));
-            document.body.scrollTop = frameTop;
+            setScrollTop(frameTop);
 
             // check if we're done!
             if(now >= end_time) {
@@ -50,12 +59,12 @@ export default function smooth_scroll_to (target, duration) {
             // If we were supposed to scroll but didn't, then we
             // probably hit the limit, so consider it done; not
             // interrupted.
-            if(document.body.scrollTop === previous_top
-                && document.body.scrollTop !== frameTop) {
+            if(getScrollTop() === previous_top
+                && getScrollTop() !== frameTop) {
                 resolve();
                 return;
             }
-            previous_top = document.body.scrollTop;
+            previous_top = getScrollTop();
 
             // schedule next frame for execution
             setTimeout(scroll_frame, 0);
